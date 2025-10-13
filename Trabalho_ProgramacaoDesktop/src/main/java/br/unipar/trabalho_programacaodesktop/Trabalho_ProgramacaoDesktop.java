cdf4a00402cafdfa5c7ec3bf50f7eb6413075a6c
Aline Hana <132148897+alinehana@users.noreply.github.com>
2025-10-13 15:02:20 -0300

    Add files via upload

---
+++ Trabalho_ProgramacaoDesktop/src/main/java/br/unipar/trabalho_programacaodesktop/Trabalho_ProgramacaoDesktop.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unipar.trabalho_programacaodesktop;
+
+/**
+ *
+ * @author aline
+ */
+public class Trabalho_ProgramacaoDesktop {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new LoginFrame().setVisible(true);
+        });
+    }
+}
